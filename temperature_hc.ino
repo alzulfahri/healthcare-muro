1e4273aec13278234568c70b4728da7557fe5b76
Alief Zulfahri <94499882+alzulfahri@users.noreply.github.com>
2024-07-19 14:35:25 +0700

    Update temperature_hc.ino

--- temperature_hc.ino
+++ temperature_hc.ino
@@ -1,3 +1,5 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
@@ -10,6 +12,9 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass the oneWire reference to DallasTemperature library
 DallasTemperature sensors(&oneWire);
 
+// Set the LCD address to 0x3F for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup(void) {
   // Start serial communication
   Serial.begin(115200);
@@ -17,15 +22,52 @@ void setup(void) {
 
   // Start up the library
   sensors.begin();
+
+  // Initialize the LCD
+  lcd.begin();
+
+  // Turn on the backlight
+  lcd.backlight();
+lcd.print("    WELCOME ");
+  lcd.setCursor(0, 1);
+  lcd.print("      TO ");
+  delay(3000);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("PSMURO");
+  lcd.setCursor(0, 1);
+  lcd.print("UNIV.GUNADARMA");
+  delay(5000);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("MCU HEALTCARE");
+  delay(3000);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Sistem monitor");
+  lcd.setCursor(0, 1);
+  lcd.print("Suhu Badan");
+  delay(5000);
+  lcd.clear();
+  lcd.print("Temperature:");
 }
 
 void loop(void) {
   // Request temperature readings
   sensors.requestTemperatures(); 
+
+  // Read temperature in Celsius
+  float tempC = sensors.getTempCByIndex(0);
+
+  // Print temperature to serial monitor
   Serial.print("Temperature is: ");
-  Serial.print(sensors.getTempCByIndex(0)); // Get temperature in Celsius
+  Serial.print(tempC);
   Serial.println("°C");
 
-  // Wait for 1 second before the next reading
+  // Print temperature to LCD
+  lcd.setCursor(0, 1); // Set cursor to second line
+  lcd.print(tempC);
+  lcd.print(" C");
+
   delay(1000);
 }
